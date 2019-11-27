c604715e6a6a64a836a6b48eea8097786ee643b9
itsmichaelsas <55444847+itsmichaelsas@users.noreply.github.com>
2019-11-27 12:18:10 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,8 +16,6 @@ function setup() {
   imageMode(CENTER);
   image(monoscopio, windowWidth/2, windowHeight/2, 1300);
 
-  //fft = new p5.FFT();
-
   analyzer = new p5.Amplitude();
   analyzer.setInput(tg);
 
@@ -36,11 +34,10 @@ function draw() {
     var volume = 0;
     volume = analyzer.getLevel();
     volume = map(volume, 0, 1, 0, height);
-    //background(202, 203, 204);
     image(monoscopio, windowWidth/2, windowHeight/2, 1300);
     ellipse(width/6,height/4,30 + volume * 2,30 + volume * 2);
+
 } else {
-    //background(202, 203, 204);
     image(monoscopio, windowWidth/2, windowHeight/2, 1300);
     ellipse(width/6,height/4,30,30);
   }
@@ -95,24 +92,14 @@ function draw() {
   fill('white');
   ellipse(width/2, height/2, windowHeight);
 
-/*
-  var spectrum = fft.analyze();
 
-  var mybass = fft.getEnergy("bass");
-  var mylowmid = fft.getEnergy("lowMid");
-  var mymid = fft.getEnergy("mid");
-  var myhighmif = fft.getEnergy("hightMid");
-  var energyArray = [mybass, mylowmid, mymid, myhighmid];
-*/
   for(var d = 100; d < windowHeight; d = d + 20) {
-    //for(var i = 0; i < spectrum.length; i++){
-      //var e = energyArray[Math.floor(Math.random() * energyArray.lenght)];
     ellipseMode(CENTER);
     stroke(0);
     strokeWeight(2);
     noFill()
     ellipse(width/2, height/2, d);
-    //}
+
   }
 
   fill(0);
